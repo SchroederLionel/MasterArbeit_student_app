0444cd5a347a00a5b20ce31d20aa6b48c054a6c0
Lionel Schroeder <schroederlionel@gmail.com>
2021-12-08 14:59:24 +0100

    adding safty string to customsnackbar

--- crayon/lib/datamodels/custom_snackbar.dart
+++ crayon/lib/datamodels/custom_snackbar.dart
@@ -4,17 +4,19 @@ import 'package:flutter/material.dart';
 class CustomSnackbar extends SnackBar {
   final BuildContext context;
   final String text;
+  final String saftyString;
   final bool isError;
   CustomSnackbar(
       {Key? key,
       required this.text,
+      required this.saftyString,
       required this.isError,
       required this.context})
       : super(
             key: key,
             backgroundColor: isError ? Colors.redAccent : Colors.greenAccent,
             content: Text(
-              AppLocalizations.of(context)!.translate(text) ?? text,
+              AppLocalizations.of(context)!.translate(text) ?? saftyString,
               style: const TextStyle(
                   color: Colors.white,
                   fontSize: 16,
