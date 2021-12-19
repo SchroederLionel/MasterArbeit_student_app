b1174dba53f25fa8a184eaf29992b8be6e8d3c72
Lionel Schroeder <schroederlionel@gmail.com>
2021-12-19 17:06:21 +0100

    changin app icon

--- crayon/lib/main.dart
+++ crayon/lib/main.dart
@@ -7,8 +7,6 @@ import 'package:crayon/providers/util/theme.dart';
 import 'package:crayon/state/enum.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:path/path.dart';
-import 'package:sqflite/sqflite.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
